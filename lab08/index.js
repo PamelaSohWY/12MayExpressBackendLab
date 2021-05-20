71df7da7401412eff6903b0f7521b25be033d380
PamelaSohWY <pamelasohwy@gmail.com>
2021-05-20 09:53:37 +0000

    Lab08 added in the js and hbs files

--- lab08/index.js
+++ lab08/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const hbs = require('hbs')
 const wax = require('wax-on')
+//add in Object ID as it is required for edit of food 
+const ObjectId = require('mongodb').ObjectId;
+
 //for Mongo 
 //short cut for the full instruction 
 const dotenv = require('dotenv');
@@ -8,14 +11,28 @@ dotenv.config(); //this function will open .env and store into Process.env , the
 
 //require('dotenv').config();
 
+async function main(){
+
 //require in MongoUtil 
-const MongoUtil = require('./MongoUtil')
+const MongoUtil = require('./MongoUtil');
+
+//require in handlebars-helpers 
+const helpers = require ('handlebars-helpers');
 
 // 1. create the express application
 let app = express();
 
-// 2. set the view engine
+// 2a. set the view engine
 app.set('view engine', 'hbs')
+
+// 2b. initialise handlebars-helpers 
+ // const helpers = require('handlebars-helpers')({
+    //     'handlebars': hbs.handlebars
+    // })    
+   
+    helpers({
+        'handlebars': hbs.handlebars
+    })
 
 // 3. where to find the public folder
 app.use(express.static('public'))
@@ -29,12 +46,14 @@ app.use(express.urlencoded({
     extended: false
 }))
 
+//6. Connect to Mongo
 // read in the environment variables
 //process.env is for environmant variables 
 //test if the database can be connected.
-MongoUtil.connect(process.env.MONGO_URI,'food_tracker')
+//function is async so that it can 
+await MongoUtil.connect(process.env.MONGO_URI,'food_tracker')
 
-// 6. routes
+// 6. Define the routes 
 
 // root route
 // this is an arrow function(optional to use )
@@ -48,7 +67,142 @@ app.get('/', (req,res)=>{
 //put morgourl in env 
 //put .env in gitirgnore
 
+//to view got to url and add '/food/add'
+// use express through a form to add information into database
+
+//to add something to database 
+app.get('/food/add', (req,res)=>{
+
+    res.render('add_food.hbs')
+})
+//process form 
+app.post('/food/add', (req,res)=>{
+    //let foodName = req.body.foodName;
+    //let calories = req.body.calories;
+   // res.send(req.body) //use this to test first
+   
+   let {foodName, calories, tags} = req.body;
+
+    // check if tags is undefined. If undefined, set it to be an empty array
+    tags = tags || [];
+
+     // if tag is a single value, convert it to be an array consisting of the value
+    // as its only element
+    tags = Array.isArray(tags) ? tags : [tags]
+
+    // 2 scenarios 
+    //Scenario 1 : only one criteria selected  
+    // assume the tags variable contains "snack"
+    // tags = Array.isArray(tags) ? tags : [tags]
+    //      => Array.isArray("snack") ? "snack" : ["snack"]
+    //   left is string and the right is an array 
+    //      => false ? "snack" : ["snack"]
+    //      => ["snack"]
+    // tags => ["snack"]
+ 
+    //Scanario 2: more than one criteria selected (2 or more)
+    // assume the tags variable contains ["unhealthy", "homecooked"]
+    // tags = Array.isArray(tags) ? tags : [tags]
+    //      => Array.isArray(["unhealthy", "homecooked"]) ? ["unhealthy", "homecooked"] : [["unhealthy", "homecooked"]]
+    // left - string  array and right - array in array (nested array) 
+    //      => true ? ["unhealthy", "homecooked"] : [["unhealthy", "homecooked"]]
+    //      => ["unhealthy", "homecooked"] 
+    // tags = ["unhealthy", "homecooked"]
+
+    //to add this doc into mongodb 
+    let db = MongoUtil.getDB();
+    //getDB function is in MongoUtil 
+    //into food collection, iu 
+    db.collection('food').insertOne({ //insertOne is used here 
+    'foodName': foodName,
+    'calories' : calories,
+    'tags': tags
+});
+//res.send("Food added")
+res.redirect('/food')
+
+})
+
+//To show all food in the database
+
+app.get('/food', async (req,res)=>{
+    let db = MongoUtil.getDB();
+    // find all the food and convert the results to an array 
+    let results = await db.collection('food').find().toArray();
+    res.render('food',{
+        'foodRecords': results 
+
+        //results i will store in food.hbs as foodRecords
+    })
+
+})
+
+
+/// display the form to edit a food item
+app.get('/food/:foodid/edit', async (req, res) => {
+    let db = MongoUtil.getDB();
+    let foodId = req.params.foodid;
+    // findOne will always give you back one object
+    let foodRecord = await db.collection('food').findOne({
+        "_id": ObjectId(foodId)
+    })
+    res.render('edit_food',{
+        foodRecord
+    })
+})
+
+
+//edit the form 
+//Note yarn add handlebars-helpers to enble in array to be used 
+app.post('/food/:foodid/edit', async(req,res)=>{
+    let { foodName, calories, tags } = req.body;
+    let foodId = req.params.foodid;
+    tags = tags || []; //if tags is undefined, it will default 
+    tags = Array.isArray(tags) ? tags : [tags]
+;
+
+//Update the document 
+//put method
+//need to specifically use UpdateOne
+let db = MongoUtil.getDB();
+await db.collection('food').updateOne({
+    '_id':ObjectId(foodId)
+},{
+    '$set': {
+        foodName, calories, tags
+    }
+})
+
+res.redirect('/food')
+})
+
+//Delete the document 
+app.get('/food/:foodid/delete', async(req,res)=>{
+    let db = MongoUtil.getDB();
+
+    let foodRecord = await db.collection('food').findOne({
+        '_id':ObjectId(req.params.foodid)
+    })
+    res.render('delete_food',{
+        foodRecord
+})
+})
+
+app.post('/food/:foodid/delete', async(req,res) =>{
+    let db = MongoUtil.getDB();
+    await db.collection('food').deleteOne({
+        '_id':ObjectId(req.params.foodid)
+    })
+   
+    res.redirect('/food')
+
+}
+)
+
 // 7. start the server
 app.listen(3000, ()=>{
     console.log("Server has started")
 })
+
+}
+main();
